60ff187c6c2a6b2e2ca63c8af283d359e662ac07
k-gourav <code2change.gourav@gmail.com>
2024-06-19 00:45:51 +0530

    Fixed calendar day layout according to day

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -1,22 +1,35 @@
 const Dates = ({ currentMonth, currentYear }) => {
   const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
-  const coloredDay = new Date()
-  const daysArray = [...Array(daysInMonth).keys()].map((i) => i + 1);
+  const coloredDay = new Date();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+  const daysArray = [
+    ...Array.from({ length: firstDayOfMonth }, () => ""),
+    ...Array.from({ length: daysInMonth }, (_, i) => i + 1),
+  ];
+  // const daysArray = [...Array(daysInMonth).keys()].map((i) => i + 1);
 
   return (
     <>
       <div className="date-container">
-        {daysArray.map((day) => (
-          (currentMonth === coloredDay.getMonth() && currentYear === coloredDay.getFullYear() && day === coloredDay.getDate()) ?
-            <div key={day} className="date-content today-color-day">
-            {day}
-          </div> : <div key={day} className="date-content">
-          {day}
-        </div>
-        ))}
+        {daysArray.map((day, index) =>
+          day === "" ? (
+            <div key={index} className="date-content empty"></div>
+          ) : currentMonth === coloredDay.getMonth() &&
+            currentYear === coloredDay.getFullYear() &&
+            day === coloredDay.getDate() ? (
+            <div key={index} className="date-content today-color-day">
+              {day}
+            </div>
+          ) : (
+            <div key={index} className="date-content">
+              {day}
+            </div>
+          )
+        )}
       </div>
     </>
   );
 };
 
 export default Dates;
+// + new Date(new Date().setDate(1)).getDay()
